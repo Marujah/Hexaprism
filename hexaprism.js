c31a608946feecfdbbc3e6d87cf105fc8abf4df0
Marouen Mhiri <marujah@gmail.com>
2017-11-28 00:15:02 +0100

    fix 3d pesentation of the grid + add rotations

--- hexaprism.js
+++ hexaprism.js
@@ -6,10 +6,18 @@
         this.grid = hexaprism.getAttribute('data-grid');
         this.childNumber = hexaprism.children.length;
         this.size = { 
-            width: parseInt(window.getComputedStyle(hexaprism.children[0]).width),
-            height: parseInt(window.getComputedStyle(hexaprism.children[0]).height)
+            width: parseInt(window.getComputedStyle(hexaprism).width),
+            height: parseInt(window.getComputedStyle(hexaprism).height)
         };
-    };
+    }
+    
+    HexaPrism.prototype.addStyles = function (element, styles) {
+        for (var prop in styles) {
+            if (element.style.hasOwnProperty(prop)) {
+                element.style[prop] = styles[prop];
+            }
+        }
+    }
 
     HexaPrism.prototype.getGrid = function (grid) {
             if (grid.match(/x/g).length !== 1) return;
@@ -22,19 +30,22 @@
     
     
     HexaPrism.prototype.gridify = function (grid, childnmr) {
-        var result = { children : [] };
         var pieceWidth =  this.size.width / grid.columns;
         var pieceHeight =  this.size.height / grid.rows;
-        var rotationAngle = 180 - (360 / childnmr);
+        var result = { 
+            grid: { columns: grid.columns , rows: grid.rows },
+            pieceSize: {
+                width: pieceWidth,
+                height: pieceHeight
+            },
+            children : [] 
+        };
+        var rotationAngle = 360 / childnmr;
         for (var count = 0; count < childnmr ; count++) {
             result.children.push({
                                     child: count,
-                                    pieceSize: { 
-                                        width: pieceWidth,
-                                        height: pieceHeight
-                                    },
                                     backgroundImage: this.prism.children[count].getAttribute('data-src'),
-                                    yRotation: -(count * rotationAngle)+'deg',
+                                    yRotation: (count * rotationAngle)+'deg',
                                     positions: []
                                 });
             for (var i = 0; i < grid.columns ; i++) {
@@ -54,34 +65,58 @@
     HexaPrism.prototype.generateGridPrism = function (data) {
         
         console.log(data);
-
-        for (var child of data.children) {
-            var pieceSize = child.pieceSize.height;
-            for (var prop in child) {
-                if (prop === 'positions') {
-                    for (var piece of child[prop]) {
-                        var newPiece = document.createElement('SPAN');
-                        newPiece.style.display = 'inline-block';
-                        newPiece.style.width = child.pieceSize.width+'px';
-                        newPiece.style.height = child.pieceSize.height+'px';
-                        newPiece.style.background = 'url('+child.backgroundImage+') no-repeat ' + piece.backgroundPositionX + ' ' + piece.backgroundPositionY + ' transparent';
-                        this.prism.children[child.child].appendChild(newPiece); 
-                    }
-                }
+        
+        var pieceSize = data.pieceSize;
+        this.prism.innerHTML = '';
+        
+        for ( var i = 0; i < (data.grid.columns * data.grid.rows); i++ ) {
+            var newSection = document.createElement('SECTION');
+            this.addStyles(newSection, {
+                perspective: '1000px',
+                position: "relative",
+                width: data.pieceSize.width+'px',
+                height: data.pieceSize.height+'px'
+            });
+            
+            var newPart = document.createElement('DIV');
+            newPart.className = 'piece';
+            this.addStyles(newPart, {
+                position: "absolute",
+                transform: "translateZ(" + -(data.pieceSize.width / 2) + "px)",
+                transformStyle: "preserve-3d",
+                width: '100%',
+                height: '100%',
+                transition: "transform .5s"
+            });
+            for (var child of data.children) {
+                var newPiece = document.createElement('FIGURE');
+                this.addStyles(newPiece, {
+                    display: 'inline-block',
+                    margin: '0',
+                    position: 'absolute',
+                    width: data.pieceSize.width+'px',
+                    height: data.pieceSize.height+'px',
+                    transformStyle: "preserve-3d",
+                    transform: "rotateY("+child.yRotation+") translateZ(" + (data.pieceSize.width / 2) + "px)",
+                    background: 'url('+child.backgroundImage+') no-repeat ' + child.positions[i].backgroundPositionX + ' ' + child.positions[i].backgroundPositionY + ' transparent',
+                    backfaceVisibility: 'hidden'
+                })
+                newPart.appendChild(newPiece);
+                newSection.appendChild(newPart);
             }
+            this.prism.appendChild(newSection);
         }
     }
     
     
     HexaPrism.prototype.init = function () {
-        var gridData = this.getGrid(this.grid);
-        // set grid styles for prism items
-        var allItems = this.prism.children;
-        for ( var i = 0; i < allItems.length; i++) {
-            allItems[i].style.display='grid';
-            allItems[i].style.gridTemplate = 'repeat('+gridData.columns+', 1fr) / repeat('+gridData.rows+', 1fr)'
-        }        
+        var gridData = this.getGrid(this.grid);  
         var data = this.gridify(gridData, this.childNumber);
+        this.addStyles(this.prism, {
+            display: 'grid',
+            gridGap: '10px',
+            gridTemplate: 'repeat('+data.grid.columns+', 1fr) / repeat('+data.grid.rows+', 1fr)'
+        })
         this.generateGridPrism(data);
     }
     
